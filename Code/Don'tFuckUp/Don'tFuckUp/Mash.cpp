e06c4545d86b277114ecafcd15a159585f14bdae
ShoGanKing <shahobkayhan@yahoo.ca>
2015-01-24 15:39:18 -0500

    mash class

---
+++ Code/Don'tFuckUp/Don'tFuckUp/Mash.cpp
@@ -0,0 +1,180 @@
+#include "Mash.h"
+#include <sstream>
+
+using namespace std;
+
+Mash::Mash() : m_Window(sf::VideoMode(1600, 900),
+    "SFML Application"),
+    m_Texture(),
+    m_Player(),
+    m_IsMovingDown(false),
+    m_IsMovingUp(false),
+    m_IsMovingLeft(false),
+    m_IsMovingRight(false),
+    m_numMash(0),
+    m_timer(240),
+    m_Timer(),
+    m_WinLose(),
+    m_Font(),
+    m_Mash()
+{
+    if (!m_Texture.loadFromFile("../../../Assets/Models/Vivi.png")) // ./ current directory, ../ back one
+    {
+        // Handle Loading Error
+    }
+
+    //gets the font for the text
+    if (!m_Font.loadFromFile("../../../Assets/Arial.ttf"));
+    {
+
+    }
+
+    //setting up the mash counter to count your mashes
+    m_Mash.setFont(m_Font);
+    m_Mash.setCharacterSize(500);
+    m_Mash.setPosition(-100, -100);
+
+    //setting up the win/lose message
+    m_WinLose.setFont(m_Font);
+    m_WinLose.setCharacterSize(500);
+    m_WinLose.setPosition(0, 300);
+
+    //setting up the time counter
+    m_Timer.setFont(m_Font);
+    m_Timer.setCharacterSize(500);
+    m_Timer.setPosition(100, 200);
+
+    m_Player.setTexture(m_Texture);
+    m_Player.setPosition(100.0f, 100.0f);
+} // end of Game()
+
+void Mash::Run()
+{
+    sf::Clock clock;
+    sf::Time timeSinceLastUpdate = sf::Time::Zero;
+    while (m_Window.isOpen())
+    {
+        ProcessEvents();
+        timeSinceLastUpdate += clock.restart();
+        while (timeSinceLastUpdate > TIME_PER_FRAME)
+        {
+            timeSinceLastUpdate -= TIME_PER_FRAME;
+            ProcessEvents();
+            Update(TIME_PER_FRAME);
+        }
+        Render();
+    }
+} // end of Run()
+
+void Mash::ProcessEvents() // Input()
+{
+    sf::Event event;
+    while (m_Window.pollEvent(event))
+    {
+        switch (event.type)
+        {
+        case sf::Event::KeyPressed:
+            HandlePlayerInput(event.key.code, true);
+            //incrementing the mashes
+            m_numMash++;
+            break;
+        case sf::Event::KeyReleased:
+            HandlePlayerInput(event.key.code, false);
+            break;
+        case sf::Event::Closed:
+            m_Window.close();
+            break;
+
+        }
+
+    }
+} // end of ProcessEvents()
+
+void Mash::Update(sf::Time aDelta)
+{
+    sf::Vector2f movement(0.0f, 0.0f);
+    if (m_IsMovingUp)
+    {
+        movement.y -= PLAYER_SPEED; // Inverted y axis for SFML...
+    }
+    if (m_IsMovingDown)
+    {
+        movement.y += PLAYER_SPEED;
+    }
+    if (m_IsMovingLeft)
+    {
+        movement.x -= PLAYER_SPEED;
+    }
+    if (m_IsMovingRight)
+    {
+        movement.x += PLAYER_SPEED;
+    }
+
+    //this is all for the countdown for timer then it checks if you hit the amount of mashes to display if you won or lost.
+    if (m_timer != 0)
+    {
+        m_timer--;
+
+        if (m_timer == 0)
+        {
+            if (m_numMash != 100)
+            {
+                m_WinLose.setString("You Lose!");
+            }
+
+            else
+            {
+                m_WinLose.setString("You Win!");
+            }
+        }
+    }
+
+    //converts int to string
+    stringstream ss;
+    stringstream tt;
+
+    tt << m_timer;
+
+    string std = tt.str();
+
+    m_Timer.setString(std);
+
+    ss << m_numMash;
+
+    string str = ss.str();
+
+    m_Mash.setString(str);
+
+    m_Player.move(movement * aDelta.asSeconds());
+} // end of Update(sf::Time aDelta)
+
+void Mash::Render() // Draw()
+{
+    m_Window.clear();
+    m_Window.draw(m_Player);
+    m_Window.draw(m_Mash);
+    m_Window.draw(m_Timer);
+    m_Window.draw(m_WinLose);
+    m_Window.display();
+} // end of Render()
+
+void Mash::HandlePlayerInput(sf::Keyboard::Key aKey, bool aIsPressed)
+{
+    if (aKey == sf::Keyboard::W)
+    {
+        m_IsMovingUp = aIsPressed;
+    }
+    else if (aKey == sf::Keyboard::S)
+    {
+        m_IsMovingDown = aIsPressed;
+    }
+    else if (aKey == sf::Keyboard::A)
+    {
+        m_IsMovingLeft = aIsPressed;
+    }
+    else if (aKey == sf::Keyboard::D)
+    {
+        m_IsMovingRight = aIsPressed;
+    }
+}
+
---
+++ Code/Don'tFuckUp/Don'tFuckUp/Mash.h
@@ -0,0 +1,44 @@
+#ifndef MASH_H
+#define MASH_H
+
+#include "Scene.h"
+#include "StaticObject.h"
+#include <SFML/Graphics.hpp>
+
+class Mash
+{
+    //Constants
+    const int PLAYER_SPEED = 100.0f;
+    const sf::Time TIME_PER_FRAME = sf::seconds(1.f / 60.f);
+
+public:
+    Mash();
+    void Run();
+
+private:
+    void ProcessEvents();
+    void Update(sf::Time delta);
+    void Render();
+    void HandlePlayerInput(sf::Keyboard::Key key, bool isPressed);
+
+private:
+    sf::RenderWindow m_Window;
+    sf::Texture m_Texture;
+    sf::Sprite m_Player;
+    sf::Font m_Font;
+    sf::Text m_Mash;
+    sf::Text m_Timer;
+    sf::Text m_WinLose;
+
+    // Member Variables
+    bool m_IsMovingUp;
+    bool m_IsMovingDown;
+    bool m_IsMovingLeft;
+    bool m_IsMovingRight;
+
+    int m_numMash;
+    int m_timer;
+
+};
+
+#endif // GAME_H
